f8a4988d5542ec0de11f0bd83f636dfe73f8bb44
Saksham <chauhanssaksham@gmail.com>
2020-06-18 15:42:24 +0530

    Changed Login Component to Uncontrolled Component

--- src/components/Pages/Login/Login.tsx
+++ src/components/Pages/Login/Login.tsx
@@ -9,24 +9,32 @@ interface State{
 }
 
 class Login extends Component<Props, State>{
+    emailInputRef: React.RefObject<HTMLInputElement>;
+    passwordInputRef: React.RefObject<HTMLInputElement>;
     constructor(props:Props){
         super(props);
+        this.emailInputRef = React.createRef<HTMLInputElement>();
+        this.passwordInputRef = React.createRef<HTMLInputElement>();
+    }
 
+    handleFormSubmit = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        console.log('this.emailInputRef: ', this.emailInputRef.current?.value);
+        console.log('this.passwordInputRef: ', this.passwordInputRef.current?.value);
     }
 
     render(){
-        
         return (
             <form className="login-form">
                 <span className="login-signup-header">Log In</span>
                 <div className="field">
-                    <input type="email" placeholder="Email" required />
+                    <input type="email" placeholder="Email" required ref={this.emailInputRef} />
                 </div>
                 <div className="field">
-                    <input type="password" placeholder="Password" required />
+                    <input type="password" placeholder="Password" required ref={this.passwordInputRef} />
                 </div>
                 <div className="field">
-                    <button>Log In</button>
+                    <button onClick={this.handleFormSubmit}>Log In</button>
                 </div>
             </form>
         );
