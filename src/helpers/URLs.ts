6a4a3c4666a5f8552b08894578becca281ae4602
Saksham <chauhanssaksham@gmail.com>
2020-07-01 04:20:26 +0530

    Changed the home page limit to 20

--- src/helpers/URLs.ts
+++ src/helpers/URLs.ts
@@ -4,7 +4,7 @@ export const APIUrls = {
     login: () => `${API_ROOT}/users/login`,
     signup: () => `${API_ROOT}/users/signup`,
     editProfile: () => `${API_ROOT}/users/edit`,
-    fetchPosts: (page: number = 1, limit: number = 5) => `${API_ROOT}/posts?page=${page}&limit=${limit}`,
+    fetchPosts: (page: number = 1, limit: number = 20) => `${API_ROOT}/posts?page=${page}&limit=${limit}`,
     fetchProfile: (user_id: string) => `${API_ROOT}/users/${user_id}`,
     userFriends: () => `${API_ROOT}/friendship/fetch_user_friends`,
 };
